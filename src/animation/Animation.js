e29a621a0a64a011aa31aaca27bb7b1b73fdafa2
Sanjeev Gwachha <gwachhas123@gmail.com>
2022-08-04 14:56:44 +0545

    updated css

--- src/animation/Animation.js
+++ src/animation/Animation.js
@@ -1,20 +1,36 @@
 import React from "react";
-import "./Animation.css"
+import "./Animation.css";
 
 const Animation = () => {
   return (
     <>
-      <div className="main-container">
+      <div className="container">
         <div className="navbar">
           <div className="menu">
             <h3 className="logo">
-              Sanjeev Gwachha
+              Brand<span>Name</span>
             </h3>
             <div className="hambuger-menu">
-                <div className="bar">
-
-                </div>
+              <div className="bar"></div>
             </div>
+          </div>
+        </div>
+        <div className="main-container">
+          <div className="main">
+            <header>
+              <div className="overlay">
+                <div className="inner">
+                  <h2 className="title">Future is here</h2>
+                  <p>
+                    lorem ipsum dolor sit amet consusjn jubsanf inasn uisanb
+                    usnc be asd asdn
+                  </p>
+                  <a href="#" className="btn">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </header>
           </div>
         </div>
       </div>
